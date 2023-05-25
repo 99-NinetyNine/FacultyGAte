4c63e4a617543240e949c0c7b4496583ba7bf5d8
sailesh <saileshshiwakoti2@gamil.com>
2023-05-25 23:32:20 +0545

    changed base dir with str

--- proj_fpms/fpms/settings.py
+++ proj_fpms/fpms/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'fpms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
