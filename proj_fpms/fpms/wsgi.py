d66c64708fbeadd089c9cf9df4e6b81dc7361973
sailesh <saileshshiwakoti2@gamil.com>
2023-07-06 18:42:29 +0545

    wsgin changed v2

--- proj_fpms/fpms/wsgi.py
+++ proj_fpms/fpms/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append("/home/FacultyGAte/proj_fpms/fpms")
+
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'fpms.settings'
 
